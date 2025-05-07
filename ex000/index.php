114c8d967c7bfc3d750c0cb05899b391a946475a
George Silva <george@agsconsultoria.com.br>
2025-05-06 22:01:20 -0400

    Aula 17 do curso de PHP do Curso em Video

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php
+            $curse = "mundo";
+            echo "Olá $curse \u{1F30E}";
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
